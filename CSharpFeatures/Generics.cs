475eb682250d10cbb2b32dd2090e8dd87e03eaf6
Wojciech Gluch <wojtekgluch@gmail.com>
2019-02-13 12:13:11 +0100

    Implemented Generics

---
+++ CSharpFeatures/Generics.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CSharpFeatures
+{
+  //opóźnienie dostarczania typu - logika oddzielona od typu
+  public class Generics
+  {
+    static void GenericsMethod()
+    {
+      Compare<string> compareString = new Compare<string>();
+
+      Console.WriteLine(compareString.CompareAny("ale", "ale"));
+    }
+  }
+
+
+  public class Compare<T>
+  {
+    public bool CompareAny(T a, T b)
+    {
+      if (a.Equals(b))
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+
+
+  }
+
+}
