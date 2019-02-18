822c390f0eddfa149cd6db05866fd2a6412f4b42
Wojciech Gluch <wojtekgluch@gmail.com>
2019-02-18 14:19:16 +0100

    Extension_methods_implemented

---
+++ ExtensionMethod/ExtensionMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ExtensionMethod
+{
+  public static class ExtensionMethods
+  {
+    public static int WordCounter(this string str)
+    {
+      string[] userText = str.Split(new char[] { ' ', '.', '?' }, StringSplitOptions.RemoveEmptyEntries);
+      int counter = userText.Length;
+      return counter;
+    }
+    public static int TotalCharactersWithoutSpace(this string str)
+    {
+      int counter = 0;
+      string[] userText = str.Split(' ');
+      foreach (var item in userText)
+      {
+        counter += item.Length;
+      }
+
+
+
+      return counter;
+    }
+  }
+}
---
+++ ExtensionMethod/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExtensionMethod
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string text = string.Empty;
+      text = "Testowe zdanie do sprawdzenia!. Policzymy różne rzeczy w stringu, które mogą okazać się ciekawe.";
+
+      Console.WriteLine(text.WordCounter());
+      Console.WriteLine(text.TotalCharactersWithoutSpace());
+      Console.ReadKey();
+    }
+  }
+}
