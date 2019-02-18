1e272523e401f812bdf76d25bf543829fa2a46d1
Wojciech Gluch <wojtekgluch@gmail.com>
2019-02-18 14:44:44 +0100

    Implemented_Generics_And_ref

---
+++ Generics/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Generics
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      var wartoscPierwsza = "wartosc pierwsza";
+      var wartoscDruga = "wartosc druga";
+
+      Replace(ref wartoscPierwsza, ref wartoscDruga); // przez ref zmieniamy wartość referenzyjnie dla pozostałej części kodu
+      Console.WriteLine("Wartość pierwsza po zamiance: " + wartoscPierwsza);
+      Console.WriteLine("Wartość druga po zamiance: " + wartoscDruga);
+
+      Console.WriteLine("-------------------------------------------------------------");
+      MergeAndShow(ref wartoscPierwsza, ref wartoscDruga);
+      Console.ReadKey();
+    }
+
+    public static void Replace<T>(ref T var1, ref T var2)
+    {
+      T helper = var1;
+      var1 = var2;
+      var2 = helper;
+    }
+
+    public static void MergeAndShow<T1, T2>(ref T1 var1, ref T2 var2)
+    {
+      Console.WriteLine("Połączone wartości po zamianie: {0} {1}", var1, var2);
+    }
+  }
+}
